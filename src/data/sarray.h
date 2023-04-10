b88aeb9d04ea7840543ae07f88b4abb34b50c4d6
Finn Eggers <finn.eggers@gmx.de>
2023-04-10 13:01:34 +0200

    fixed cpu/gpu_pointers visibility

--- src/data/sarray.h
+++ src/data/sarray.h
@@ -26,7 +26,7 @@ class SArray : public Array<TYPE> {
     using CPtr         = std::shared_ptr<CPUArrayTYPE>;
     using GPtr         = std::shared_ptr<GPUArrayTYPE>;
 
-    protected:
+    public:
     CPtr cpu_values = nullptr;
     GPtr gpu_values = nullptr;
 
