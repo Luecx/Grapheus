d85ed4944c999395fcbd7be35323dd4970df3792
Jay Honnold <jayhonnold@gmail.com>
2024-03-03 10:39:05 -0700

    Revert "Fully quantized with CReLU"
    This reverts commit 95f82c428a451764902292a095a83d0497c92f5b.

--- src/nn/layers/affine_multi.h
+++ src/nn/layers/affine_multi.h
@@ -6,16 +6,7 @@ namespace nn {
 
 struct AffineMulti : public nn::Affine {
 
-    AffineMulti(Layer* prev, size_t size, size_t batches)
-        : Affine(prev, size * batches) {
-
-        for (size_t i = size; i < this->size; i++)
-            for (size_t j = 0; j < prev->size; j++)
-                weights.values(i, j) = weights.values(i % size, j);
-
-        weights.values >> data::GPU;
-        bias.values >> data::GPU;
-    }
+    AffineMulti(Layer* prev, size_t size, size_t counts)
+        : Affine(prev, size * counts) {}
 };
-
 }    // namespace nn
